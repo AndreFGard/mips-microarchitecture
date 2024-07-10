22c6b2a4c92500ed331ddf45b7536bc66abb52e3
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-07-10 14:29:55 -0300

    add full pc cycle

--- design.sv
+++ design.sv
@@ -1,14 +1,18 @@
-module pcplus4(input [4:0]last_instr, output [4:0] next_instr);
-  assign next_instr = last_instr + 4;
-endmodule
-
 module pcplus4(input [INSTRSIZE:0]last_instr, output [INSTRSIZE:0] next_instr);
-  parameter INSTRSIZE = 3;
+  parameter INSTRSIZE = 4;
   assign next_instr = last_instr + INSTRSIZE;
 endmodule
 
-module pc_multiplexer(input [3:0] plus4_instr, input [3:0] branch, input use_branch, output [3:0] next_instr );
+
+module pc_multiplexer(input [4:0] plus4_instr, input [4:0] branch, input use_branch, output [4:0] next_instr );
 
 assign next_instr = (use_branch) ? branch : plus4_instr;
   
+endmodule
+
+
+module pc(input [4:0] next_instr, input clk, output reg [4:0] curr_instr );
+  always @(posedge clk) begin
+    curr_instr = next_instr;
+  end
 endmodule
--- testbench_instruction.sv
+++ testbench_instruction.sv
@@ -22,21 +22,25 @@ endmodule
 
 ///////// pc_multiplexer
 module ttb;
-  reg [3:0] last_instr = 5;
-  reg [3:0] branch = 9;
+  reg [4:0] last_instr = 5;
+  reg [4:0] branch = 9;
   reg usebranch = 0;
-  wire [3:0] next_instr;
+  wire [4:0] next_instr;
+  reg [4:0] next_next_instr;
+  reg clk = 0;
   pc_multiplexer pcp4 (last_instr, branch, usebranch, next_instr);
+  pc papapapa (next_instr, clk, next_next_instr);
 
+  initial forever #5 clk = ~clk;
 initial begin 
 	
     #5 usebranch = 0;
-   #5 usebranch = 1;
+    #5 usebranch = 1;
 end
 
   
   initial forever begin
-  	#5 $display("last %d mext %d", last_instr, next_instr);
+    #5 $display("last %d mext %d pc_instr %d", last_instr, next_instr, next_next_instr);
   end
   
   
