5e462a551202ac48a4fa8fbec4195780f4beba59
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-07-10 14:16:45 -0300

    add pc pluys 4

--- design.sv
+++ design.sv
@@ -1,3 +1,14 @@
 module pcplus4(input [4:0]last_instr, output [4:0] next_instr);
   assign next_instr = last_instr + 4;
 endmodule
+
+module pcplus4(input [INSTRSIZE:0]last_instr, output [INSTRSIZE:0] next_instr);
+  parameter INSTRSIZE = 3;
+  assign next_instr = last_instr + INSTRSIZE;
+endmodule
+
+module pc_multiplexer(input [3:0] plus4_instr, input [3:0] branch, input use_branch, output [3:0] next_instr );
+
+assign next_instr = (use_branch) ? branch : plus4_instr;
+  
+endmodule
--- testbench_instruction.sv
+++ testbench_instruction.sv
@@ -19,3 +19,28 @@ end
     #70 $finish;
   end
 endmodule
+
+///////// pc_multiplexer
+module ttb;
+  reg [3:0] last_instr = 5;
+  reg [3:0] branch = 9;
+  reg usebranch = 0;
+  wire [3:0] next_instr;
+  pc_multiplexer pcp4 (last_instr, branch, usebranch, next_instr);
+
+initial begin 
+	
+    #5 usebranch = 0;
+   #5 usebranch = 1;
+end
+
+  
+  initial forever begin
+  	#5 $display("last %d mext %d", last_instr, next_instr);
+  end
+  
+  
+  initial begin
+    #70 $finish;
+  end
+endmodule
