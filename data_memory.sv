ed94b30cf9f40649352dcfa52f3feaa6b47e96a9
Wenderson Juvenal <125162176+wenderson-juvenal@users.noreply.github.com>
2024-07-12 21:56:35 -0400

    Create data_memory.sv

---
+++ data_memory.sv
@@ -0,0 +1,16 @@
+module data_memory (
+  input clk,
+  input [4:0] A,
+  input [31:0] WD,
+  input WE,
+  output reg [31:0] RD
+);
+  reg [31:0] Mem [31:0]; // temporário (substituir pelo módulo q manipula a memória)
+  
+  always @ (posedge clk) begin
+    if (WE)
+      Mem[A] <= WD; 
+    else
+      RD <= {Mem[A], Mem[A+1], Mem[A+2], Mem[A+3]};
+  end
+endmodule;
