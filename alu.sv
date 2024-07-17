c4556ed3ff3ae4a7bb1fac17d02cdbb35ca21e9d
Wenderson Juvenal <125162176+wenderson-juvenal@users.noreply.github.com>
2024-07-17 13:01:56 -0300

    Create alu.sv

---
+++ alu.sv
@@ -0,0 +1,23 @@
+module ALU(
+  input [2:0] aluControl, 
+  input [31:0] srcA, 
+  input [31:0] srcB, 
+  output zero, 
+  output [31:0] aluResult 
+);
+
+  parameter ADD = 3'b010; 
+  parameter SUBT = 3'b110; 
+  parameter AND = 3'b000; 
+  parameter OR = 3'b001; 
+  parameter SETLESSTHAN = 3'b111;
+  
+  assign aluResult = (aluControl == ADD) ? (srcA + srcB): 
+                     (aluControl == SUBT) ? (srcA - srcB): 
+                     (aluControl == AND) ? (srcA && srcB): 
+                     (aluControl == OR) ? (srcA || srcB): 
+                     (aluControl == SETLESSTHAN) ? (srcA < srcB): 32'd0;
+  
+  assign zero = (aluResult == 0) ? 1 : 0;
+  
+endmodule
