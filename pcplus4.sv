e80d83c5d8fe168d70aecef0bad568cd03454cc6
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-07-24 13:43:26 -0300

    Update pcplus4.sv

--- pcplus4.sv
+++ pcplus4.sv
@@ -2,7 +2,7 @@ module pcplus4(
   input [INSTRSIZE:0]last_instr, 
   output [INSTRSIZE:0] next_instr
 );
-  parameter INSTRSIZE = 4;
+  parameter INSTRSIZE = 31;
   
   assign next_instr = last_instr + INSTRSIZE;
 
