64a03d9efe714aa1649b329d641edad2e0d54fe3
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-07-24 13:42:49 -0300

    Update pc_src_mux.sv

--- pc_src_mux.sv
+++ pc_src_mux.sv
@@ -1,9 +1,8 @@
 module pc_src_mux (
-  parameter INSIZE = 31;
   input PCSrc,
-  input [INSIZE:0] PCPlus4,
-  input [INSIZE:0] PCBranch,
-  output [INSIZE:0] Result
+  input [31:0] PCPlus4,
+  input [31:0] PCBranch,
+  output [31:0] Result
 );
 
  assign Result = PCSrc ? PCBranch : PCPlus4;
