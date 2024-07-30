d1a3f5c4e4841599ed04f757b17efaaa77663534
Wenderson Juvenal <125162176+wenderson-juvenal@users.noreply.github.com>
2024-07-30 16:42:27 -0300

    Update pc_branch.sv

--- pc_branch.sv
+++ pc_branch.sv
@@ -1,9 +1,9 @@
 module pc_branch(
   
   // juntou <<2 e pcbranch
-  input [31:0] SignImm,
+  input signed [31:0] SignImm,
   input [31:0] PCplus4,
-  output [31:0] result
+  output signed [31:0] result
 );
   
   assign result = PCplus4 + SignImm*4; 
