214c3fd9a8347883d54329b32f3b359ff9ab8326
Wenderson Juvenal <125162176+wenderson-juvenal@users.noreply.github.com>
2024-07-30 11:05:56 -0300

    Update data_memory.sv

--- data_memory.sv
+++ data_memory.sv
@@ -1,17 +1,11 @@
 module data_memory (
   input clk,
-  input [31:0] A,
-  input [31:0] WD,
+  input signed [31:0] A,
+  input signed [31:0] WD,
   input WE,
-  output reg [31:0] RD
+  output reg signed [31:0] RD
 );
-  reg [31:0] Mem [31:0];
-
-  integer i;
-  initial begin
-    for (i = 0; i < 32; i = i + 1) 
-      Mem[i] = 32'd0;
-  end
+  reg signed[31:0] Mem [31:0];
   
   always @ (posedge clk) begin
     if (WE)
@@ -20,3 +14,4 @@ module data_memory (
       RD <= Mem[A];
   end
 endmodule;
+
