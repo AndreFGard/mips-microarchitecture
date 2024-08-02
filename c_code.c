9d45839ede43cf2342fad89a0271c3dac09b539b
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2024-08-02 08:43:13 -0300

    add high level program

---
+++ c_code.c
@@ -0,0 +1,11 @@
+int main(){
+    int current_value = 10;
+    int add_per_loop = 20;
+    int target = 50;
+    int loop_counter = 0;
+    while (current_value != target){
+        current_value += add_per_loop;
+        loop_counter += 1;
+    }
+}
+
